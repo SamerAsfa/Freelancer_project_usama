84e455a37519eca029231150f8455761bb6f3d9c
samer asfa <samerasfa@gmail.com>
2022-07-28 23:37:24 +0300

    not doen

--- app/src/main/java/com/example/myapplication/myapplication/data/remote/api/RetrofitApi.kt
+++ app/src/main/java/com/example/myapplication/myapplication/data/remote/api/RetrofitApi.kt
@@ -1,15 +1,14 @@
 package com.example.myapplication.myapplication.data.remote.api
 
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface RetrofitApi {
 
 
- /*
-    @FormUrlEncoded
-    @POST("login.php")
-    suspend fun user_auth_login(
-        @Field("username") username: String,
-        @Field("password") password: String,
-        @Field("code") code: String
-    ): User_Remote_Dto
-  */
+    @GET("companyById/123456")
+    suspend fun getUserNameById(
+        @Query("id") id: String,
+    ): String
+
 }
